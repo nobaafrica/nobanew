e29c354db47d3768e952b2d250e40a196e8105d7
ifedeejoy <ifedeejoy@gmail.com>
2021-11-15 13:12:47 +0100

    removed regex from phone number validation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,7 +35,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'phoneNumber' => ['required', 'unique:users', 'regex:/^[0-9]{15}$/'],
+            'phoneNumber' => ['required', 'unique:users'],
             'email' => 'required|string|email|max:255|unique:users',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
