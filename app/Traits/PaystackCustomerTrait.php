ee50b85c4627a0b8b4ece344ca404cd693517d01
ifedeejoy <ifedeejoy@gmail.com>
2021-12-11 13:26:09 +0100

    changed paystack create profile request variablee from $requeest to $apiRequest

--- app/Traits/PaystackCustomerTrait.php
+++ app/Traits/PaystackCustomerTrait.php
@@ -12,14 +12,14 @@ trait PaystackCustomerTrait
     public function createCustomerProfile(object $user)
     {
         $url = "https://api.paystack.co/customer";
-        $request = Http::withToken(config('app.paystack_secret'))->post($url, [
+        $apiRequest = Http::withToken(config('app.paystack_secret'))->post($url, [
             'first_name' => $user->firstName,
             'last_name' => $user->lastName,
             'phone' => $user->phoneNumber,
             'email' => $user->email,
         ])->object();
-        if($request->status == 'true'):
-            $customer = $request->data->customer_code;
+        if($apiRequest->status == 'true'):
+            $customer = $apiRequest->data->customer_code;
             $wallet = Wallet::where('user_id', $user->id)->first();
             if($wallet):
                 $user->wallet()->update([
@@ -33,7 +33,7 @@ trait PaystackCustomerTrait
             endif;
             return $this->verifyCustomer($customer, $user);
         else:
-            session()->flash('error', $request->message);
+            session()->flash('error', $apiRequest->message);
         endif;
     }
 
